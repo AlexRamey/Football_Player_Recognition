5f4c163ac564eea663e856086218675aec3683dc
AlexRamey <alex.b.ramey@gmail.com>
2020-04-04 18:56:35 -0400

    split football dataset into train (70%) and test (30%)

---
+++ data/train_test_split.py
@@ -0,0 +1,24 @@
+import json
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+with open('data/jersey_number_labelling_via_project.json') as f:
+    viaProject = json.load(f)
+
+keys = list(viaProject["_via_img_metadata"].keys())
+indices = np.arange(start=0, stop=len(keys), step=1)
+placeholders = np.zeros(len(keys))
+x_train, x_test, y_train, y_test = train_test_split(indices, placeholders, test_size=.3)
+
+football_train = list(map(lambda idx: viaProject["_via_img_metadata"][keys[idx]]["filename"], x_train))
+football_test = list(map(lambda idx: viaProject["_via_img_metadata"][keys[idx]]["filename"], x_test))
+
+data = {}
+data['train'] = football_train
+data['num_train'] = len(football_train)
+data['test'] = football_test
+data['num_test'] = len(football_test)
+
+with open('data/football_train_test_split.json', 'w') as outfile:
+    json.dump(data, outfile)
