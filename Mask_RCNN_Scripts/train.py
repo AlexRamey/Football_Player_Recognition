5774f66fa66463db88c05354229030217752db4d
Alex Ramey <alex.b.ramey@gmail.com>
2020-04-11 22:14:56 +0000

    support training from previously learned weights

--- Mask_RCNN_Scripts/train.py
+++ Mask_RCNN_Scripts/train.py
@@ -8,13 +8,11 @@ from mrcnn.utils import download_trained_weights
 from FootballDataset import FootballDataset
 from SVHNDataset import SVHNDataset
 
-if (len(sys.argv) != 3):
-    sys.exit("2 cmd line arguments are required: train set [svhn|football] and test set [svhn|football]")
+if (len(sys.argv) < 2):
+    sys.exit("1 cmd line argument required: train set [svhn|football]")
 
 if sys.argv[1] != 'svhn' and sys.argv[1] != 'football':
     sys.exit("Invalid train arg. Got " + sys.argv[1] + ", expected svhn or football")
-if sys.argv[2] != 'svhn' and sys.argv[2] != 'football':
-    sys.exit("Invalid test arg. Got " + sys.argv[2] + ", expected svhn or football")
 
 # train set and validation set
 if sys.argv[1] == 'svhn':
@@ -46,12 +44,15 @@ config.display()
 model = MaskRCNN(mode='training', model_dir='./', config=config)
 
 # Local path to trained weights file
-COCO_MODEL_PATH = "./mask_rcnn_coco.h5"
-# Download COCO trained weights from Releases if needed
-if not os.path.exists(COCO_MODEL_PATH):
-    download_trained_weights(COCO_MODEL_PATH)
-
-model.load_weights(COCO_MODEL_PATH, by_name=True, exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",  "mrcnn_bbox", "mrcnn_mask"])
+if (len(sys.argv) == 3):
+    model.load_weights(sys.argv[2], by_name=True)
+else:
+    COCO_MODEL_PATH = "./mask_rcnn_coco.h5"
+    # Download COCO trained weights from Releases if needed
+    if not os.path.exists(COCO_MODEL_PATH):
+        download_trained_weights(COCO_MODEL_PATH)
+    model.load_weights(COCO_MODEL_PATH, by_name=True, exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",  "mrcnn_bbox", "mrcnn_mask"])
 
 # train weights (output layers or 'heads')
 model.train(train_set, validation_set, learning_rate=config.LEARNING_RATE, epochs=5, layers='heads')
+
